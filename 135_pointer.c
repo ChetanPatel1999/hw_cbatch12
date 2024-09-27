63aa9541f9df663e680c3ca85c8c8592f18d02c6
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-09-27 09:16:32 +0530

    27/09

--- 135_pointer.c
+++ 135_pointer.c
@@ -9,5 +9,5 @@ void main()
   printf("adress of a by ptr= %d\n",ptr);//1234
   ptr++;
   printf("adress of a = %d\n",&a);//1234
-  printf("adress of a by ptr= %d\n",*ptr);//1238
+  printf("adress of a by ptr= %d\n",ptr);//1238
 }
---
+++ 138_daynamic_memory.c
@@ -0,0 +1,27 @@
+// dyanamic memory allocation by malloc
+#include <stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int *ptr,*temp;
+    int n;
+    printf("enter dyanamic array size : ");
+    scanf("%d",&n);
+    ptr = (int*)malloc(n*sizeof(int)); // 400
+    temp=ptr;
+    int i;
+    for (i = 0; i < n; i++)//5
+    {
+        scanf("%d",ptr);//416
+        ptr++;//420
+    }
+    ptr=temp;//400
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", *ptr);
+        ptr++;
+    }
+    ptr=temp;
+    free(ptr);
+    //we write also code belove ....
+}
